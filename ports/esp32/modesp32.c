aa60db34b8e5fb79e0d7bd9b3601531c19646f46
andineise <69535395+andineise@users.noreply.github.com>
2024-03-14 13:09:38 +0100

    Update modesp32.c
    1
    
    Signed-off-by: andineise <69535395+andineise@users.noreply.github.com>

--- ports/esp32/modesp32.c
+++ ports/esp32/modesp32.c
@@ -205,15 +205,19 @@ static const mp_rom_map_elem_t esp32_module_globals_table[] = {
     #endif
     { MP_ROM_QSTR(MP_QSTR_idf_heap_info), MP_ROM_PTR(&esp32_idf_heap_info_obj) },
 
+    #if MICROPY_HW_ENABLE_CAN
+    { MP_ROM_QSTR(MP_QSTR_CAN), MP_ROM_PTR(&esp32_can_type) },
+    #endif
+
     { MP_ROM_QSTR(MP_QSTR_NVS), MP_ROM_PTR(&esp32_nvs_type) },
     { MP_ROM_QSTR(MP_QSTR_Partition), MP_ROM_PTR(&esp32_partition_type) },
     { MP_ROM_QSTR(MP_QSTR_RMT), MP_ROM_PTR(&esp32_rmt_type) },
     #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
     { MP_ROM_QSTR(MP_QSTR_ULP), MP_ROM_PTR(&esp32_ulp_type) },
     #endif
-	#if (MICROPY_HW_ENABLE_CAN) && (ESP_IDF_VERSION_MAJOR >= 4) && (ESP_IDF_VERSION_MINOR >= 2)
-    { MP_ROM_QSTR(MP_QSTR_CAN), MP_ROM_PTR(&esp32_can_type) },
-    #endif
+
+
+
     { MP_ROM_QSTR(MP_QSTR_WAKEUP_ALL_LOW), MP_ROM_FALSE },
     { MP_ROM_QSTR(MP_QSTR_WAKEUP_ANY_HIGH), MP_ROM_TRUE },
 
